d3a186075814f1ccf1d48476374a3b9933200e67
Daniel Nguyen <danieltvnguyen@gmail.com>
2024-11-22 15:51:04 -0500

    added drake

--- stepping_stone.h
+++ stepping_stone.h
@@ -1,10 +1,27 @@
 #include "plane.h"
+#include <Eigen/Dense>
+#include <drake/math/rigid_transform.h>
 
 // Derived class
-class SteppingStone : public Plane 
+class SteppingStone
 {
     public:
-        SteppingStone(Eigen::Vector3d point, Eigen::Vector3d normal, float width, float height);
+        SteppingStone(drake::math, float width, float height);
         ~SteppingStone();
+
+
+        // Halfspace representation for the stepping stone
+        Eigen::Matrix<double, 6, 3> getConstraints();
+    
+    private:
+        Eigen::Vector3d point;
+        Eigen::Vector3d normal;
+        float width;
+        float height;
+        
+
+        Eigen::Matrix<double, 6, 3> A;
+            
+    
 
 };
